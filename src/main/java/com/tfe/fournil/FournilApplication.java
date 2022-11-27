17340c597a20ecfdda3e769be4895d2d20334cd9
jerde89 <81580392+jerde89@users.noreply.github.com>
2022-11-27 10:39:28 +0100

    Update FournilApplication.java

--- src/main/java/com/tfe/fournil/FournilApplication.java
+++ src/main/java/com/tfe/fournil/FournilApplication.java
@@ -2,17 +2,21 @@ package com.tfe.fournil;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 
 @Slf4j
-@SpringBootApplication
-@ComponentScan("com.tfe.fournil.controller")
+//@SpringBootApplication
+@Configuration
+@ComponentScan("com.tfe.fournil")
+@EnableAutoConfiguration
 public class FournilApplication {
 
-   
+
     public static void main(String[] args) {
         log.info("!!!!!!!!!!API IS READY!!!!!!!!!!");
         SpringApplication.run(FournilApplication.class, args);
