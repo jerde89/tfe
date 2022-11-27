fadef2ae5103e4d95671113fefcbad6d8975e2e3
jerde89 <81580392+jerde89@users.noreply.github.com>
2022-11-27 11:25:01 +0100

    Update WelcomeController.java

--- src/main/java/com/tfe/fournil/controller/WelcomeController.java
+++ src/main/java/com/tfe/fournil/controller/WelcomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
-@RequestMapping("/welcome")
+@RequestMapping("")
 public class WelcomeController {
     @GetMapping("")
     public String welcomeAppel() {
