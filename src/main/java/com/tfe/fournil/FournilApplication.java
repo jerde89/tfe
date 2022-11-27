81bb847224dc663d72a37ce1e8dba158785f65b1
jerde89 <81580392+jerde89@users.noreply.github.com>
2022-11-27 10:26:01 +0100

    Update FournilApplication.java

--- src/main/java/com/tfe/fournil/FournilApplication.java
+++ src/main/java/com/tfe/fournil/FournilApplication.java
@@ -3,25 +3,21 @@ package com.tfe.fournil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @Slf4j
 @SpringBootApplication
+@ComponentScan("com.tfe.fournil.controller")
 public class FournilApplication {
 
+   
     public static void main(String[] args) {
         log.info("!!!!!!!!!!API IS READY!!!!!!!!!!");
         SpringApplication.run(FournilApplication.class, args);
               }
-    @GetMapping("")
-    public String showContact()
-    {
 
-        log.info("test Controller");
-        //Nom de la JSP
-        return "contact";
-
-    }
     }
 
 
