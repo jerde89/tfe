9054d2da677f83d84f00522ad1909a812ed444a4
jerde89 <81580392+jerde89@users.noreply.github.com>
2022-11-27 10:46:33 +0100

    Update FournilApplication.java

--- src/main/java/com/tfe/fournil/FournilApplication.java
+++ src/main/java/com/tfe/fournil/FournilApplication.java
@@ -2,18 +2,13 @@ package com.tfe.fournil;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 
 @Slf4j
-//@SpringBootApplication
-@Configuration
-@ComponentScan("com.tfe.fournil")
-@EnableAutoConfiguration
+@SpringBootApplication
+
 public class FournilApplication {
 
 
